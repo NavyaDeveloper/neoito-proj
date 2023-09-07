ba93b9c1dd80660c8132bff6cb5baa02282c2536
Navya <101489443+NavyaDeveloper@users.noreply.github.com>
2023-09-07 15:42:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World\n'
+  const msg = 'Hello World NodeJS\n'
   res.end(msg);
 });
 
